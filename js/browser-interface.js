1fec7df61a57cee48609ff0e3fa2194277cb4830
Niem Nguyen and Pablo Vera <niem@>
2016-05-23 16:06:37 -0700

    add a comment for the version of this project that prints to the console

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -8,6 +8,6 @@ var Journal = require ('./../js/journal.js').Journal;
     var bodyMessage = $('#user-message').val();
 
     var newJournal = new Journal(titleMessage, bodyMessage);
-    $('#solution').append(newJournal.read());
+    $('#solution').append(newJournal.read()); // if i want this to print to the console change this line to console.log(newJournal.read)
   });
 });
